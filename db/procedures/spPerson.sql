caf5fe916fd2bf0c165015605ed058e7c82a51e7
Gustas2077 <333160@student.mboutrecht.nl>
2023-02-24 15:05:25 +0100

    Procedure: Persons

--- db/procedures/spPerson.sql
+++ db/procedures/spPerson.sql
@@ -1,22 +1,54 @@
-DROP PROCEDURE IF EXISTS spAddPerson;
-
-DELIMITER //
-
-CREATE PROCEDURE spAddPerson
-(
-  firstname varchar(60), 
-  infix varchar(30),
-  lastname varchar(50)
-  dateOfBirth date
-) 
-BEGIN
-    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
-    BEGIN 
-        ROLLBACK;
-        SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
-    END;
-    START TRANSACTION;
-        INSERT INTO Person (firstname, infix, lastname, dateOfBirth) VALUES (firstname, infix, lastname, dateOfBirth);
-    COMMIT;
-END //
-DELIMITER ;
+-- Name: CreatePerson
+-- Input: firstname, infix, lastname, dateOfBirth
+-- Output: id
+CREATE PROCEDURE CreatePerson(
+    IN firstname varchar(60),
+    IN infix varchar(30),
+    IN lastname varchar(50),
+    IN dateOfBirth date
+) BEGIN -- Rollback if person already exists
+DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
+SELECT 0 AS id;
+END;
+START TRANSACTION;
+INSERT INTO Person (firstname, infix, lastname, dateOfBirth)
+VALUES (firstname, infix, lastname, dateOfBirth);
+SELECT LAST_INSERT_ID() AS id;
+COMMIT;
+END;
+-- Name: UpdatePerson
+-- Input: id or firstname or infix or lastname or dateOfBirth
+-- Output: id
+CREATE PROCEDURE UpdatePerson(
+    IN id int,
+    IN firstname varchar(60),
+    IN infix varchar(30),
+    IN lastname varchar(50),
+    IN dateOfBirth date
+) BEGIN -- Rollback if person not exists
+DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
+SELECT 0 AS id;
+END;
+START TRANSACTION;
+UPDATE Person
+SET firstname = firstname,
+    infix = infix,
+    lastname = lastname,
+    dateOfBirth = dateOfBirth
+WHERE id = id;
+SELECT id;
+COMMIT;
+END;
+-- Name: DeletePerson
+-- Input: id
+-- Output: id
+CREATE PROCEDURE DeletePerson(IN id int) BEGIN -- Rollback if person not exists
+DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
+SELECT 0 AS id;
+END;
+START TRANSACTION;
+DELETE FROM Person
+WHERE id = id;
+SELECT id;
+COMMIT;
+END;
