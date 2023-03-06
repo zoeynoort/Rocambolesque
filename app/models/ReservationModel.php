c8226f30bb3fa122bce81751f7abd73f3cfec325
Robijn Noort <rrnoort@gmail.com>
2023-03-06 14:40:33 +0100

    feat: added methods for creating and deleting

--- app/models/ReservationModel.php
+++ app/models/ReservationModel.php
@@ -10,11 +10,24 @@
         }
 
         public function create($personId, $timeslot, $table, $day, $adults, $childs) {
-            $this->db->query("CALL   ")
+            $this->db->query("CALL spReservationAdd(:personId, :timeslot, :tableId, :day, :adults, :childs);");
+            
+            $this->db->bind(":personId", $personId);
+            $this->db->bind(":timeslot", $timeslot);
+            $this->db->bind(":table", $table);
+            $this->db->bind(":day", $day);
+            $this->db->bind(":adults", $adults);
+            $this->db->bind(":children", $children);
+
+            $this->db->execute();
+
         }
 
         public function delete($id) {
-            $this->db->query("call");
-            $this->db->bind(":id", )
+            $this->db->query("CALL spReservationDelete(:id);");
+            
+            $this->db->bind(":id", $id);
+            
+            $this->db->execute()
         }
     }
