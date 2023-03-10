c55ed899b3e0d17316e7dfa49bd1ef39c2cf1724
Robijn Noort <rrnoort@gmail.com>
2023-03-10 14:22:47 +0100

    feata: added getUserById

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -8,6 +8,7 @@
         {
             $this->db = new Database();
         }
+
         public function getUserByUsername($username) 
         {
             $this->db->query("SELECT * FROM User WHERE username = :username;");
@@ -15,5 +16,21 @@
             $this->db->bind(":username", $username);
 
             return $this->db->single();
+        }
+        
+        public function getUserById($id) 
+        {
+            $this->db->query("SELECT * FROM User WHERE id = :id;");
+
+            $this->db->bind(":id", $id);
+
+            return $this->db->single();
+        }
+
+        public function createUser($data) 
+        {
+            $this->db->query("CALL ");
+
+            $this->db->bind("");
         }
     }
