515f9709ee07af9e1ed7ad44d1a43e5065c24ddd
Robijn Noort <rrnoort@gmail.com>
2023-03-13 14:45:40 +0100

    refactor: changed user procedures

--- db/procedures/user.sql
+++ db/procedures/user.sql
@@ -1,55 +1,47 @@
--- Name: CreateUser
--- Input: id, personId, username, password
--- Output: id
-DELIMETER / / CREATE PROCEDURE CreateUser(
-    IN id INT,
-    IN personId INT,
-    IN username VARCHAR(60),
-    IN password VARCHAR(256)
-) BEGIN -- Rollback if person already exists
-DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
-SELECT 0 AS id;
-END;
-START TRANSACTION;
-INSERT INTO User (id, personId, username, password)
-VALUES (id, personId, username, password);
-SELECT LAST_INSERT_ID() AS id;
-COMMIT;
-END;
-DELIMITER;
--- Name: UpdateUser
--- Input: id or personId or username or password
--- Output: id
-DELIMETER / / CREATE PROCEDURE UpdateUser(
-    IN id INT,
-    IN personId INT,
-    IN username VARCHAR(60),
-    IN password VARCHAR(256)
-) BEGIN -- Rollback if person not exists
-DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
-SELECT 0 AS id;
-END;
-DELIMETER;
-START TRANSACTION;
-UPDATE User
-SET personId = personId,
-    username = username,
-    password = password
-WHERE id = id;
-SELECT id;
-COMMIT;
-END;
--- Name: DeleteUser
--- Input: id
--- Output: id
-DELIMETER / / CREATE PROCEDURE DeleteUser(IN id int) BEGIN -- Rollback if person not exists
-DECLARE EXIT HANDLER FOR 1062 BEGIN ROLLBACK;
-SELECT 0 AS id;
-END;
-DELIMETER;
-START TRANSACTION;
-DELETE FROM User
-WHERE id = id;
-SELECT id;
-COMMIT;
-END;
+DROP PROCEDURE IF EXISTS spUserAdd;
+
+DELIMITER //
+
+CREATE PROCEDURE spUserAdd
+(
+    firstname varchar(60),
+    infix varchar(30),
+    lastname varchar(50),
+    dateOfBirth date,
+    username varchar(60),
+    password varchar(256)
+) 
+BEGIN
+    DECLARE personId 	INT UNSIGNED DEFAULT 0;
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN 
+        ROLLBACK;
+        SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+    END;
+    START TRANSACTION;
+        INSERT INTO Person (firstname, infix, lastname, dateOfBirth) VALUES (firstname, infix, lastname, dateOfBirth);
+        SET personId = LAST_INSERT_ID();
+        INSERT INTO User (personId, username, password) VALUES (personId, username, password);
+    COMMIT;
+END //
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS spUserDelete;
+
+DELIMITER //
+
+CREATE PROCEDURE spUserDelete
+(
+   id int
+) 
+BEGIN
+    DECLARE EXIT HANDLER FOR SQLEXCEPTION 
+    BEGIN 
+        ROLLBACK;
+        SELECT 'An error has occurred, operation rollbacked and the stored procedure was terminated';
+    END;
+    START TRANSACTION;
+        DELETE FROM User WHERE id = id;
+    COMMIT;
+END //
+DELIMITER ;
