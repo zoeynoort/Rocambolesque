0f3329a08a723b3fb52c2e49a384e441ca364981
fdschepper <333476@student.mboutrecht.nl>
2023-03-13 15:10:17 +0100

    css for login page cool

--- app/views/login.php
+++ app/views/login.php
@@ -1,11 +1,30 @@
-<body>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rocambolesque</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+
+<body id="login">
     <!-- Header-->
     <?php $this->view("partials/header"); ?>
 
     <!-- Body -->
+    <div class="frame">
+        <h1>log in</h1>
+        <a href="<?php URLROOT; ?>/signup">no account?</a>
 
-    <h1>log in</h1>
-    <a href="<?php URLROOT; ?>/signup">no account?</a>
+        <form action="<?= URLROOT; ?>/countries/update" method="post">
+            <input type="text" name="Email" value="" placeholder="Email">
+
+            <input type="text" name="Password" value="" placeholder="Password">
+
+            <input class="enterbtn" type="submit" name="submit" value="Enter">
+        </form>
+
+    </div>
+
 
     <!-- Footer -->
     <?php $this->view("partials/footer"); ?>
